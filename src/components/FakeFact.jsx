4ea5409a28e68e0f7e3c8d7c8f9fc8f4a9dc65ee
Adam Bedient <adambedient@gmail.com>
2024-07-22 17:07:17 -0400

    Rough edit the layout for top two thirds

--- src/components/FakeFact.jsx
+++ src/components/FakeFact.jsx
@@ -43,13 +43,14 @@ function FakeFact(props) {
   }
 
   useEffect(() => {
-    formData ? generateFact() : setAnswer('You have not submitted and answer. Here is the default answer to use for page styling.')
+    formData ? generateFact() : setAnswer('You have not submitted and answer. Here is the default answer to use for page styling.<br></br>Feel free to use this space to detirmine how things should be styled.<br></br>Because the landspeed of an African swallow is pretty darn slow! Or are they fast? I do not know much about biology, or I suppose ecology. Is studying birds a part of ecology? I know its ornithology, but I also think its part of ecology. Or at least a part of being a naturalist. Or at least a part of being an interesting person.')
   }, [])
 
 
 
   return (
-    <main className="flex flex-col items-center">
+    <main className="w-full h-screen bg-gray-100">
+      <div className="flex flex-col items-center h-2/3 mb-3 bg-white relative">
         {/* <header>
           <img src="/lines.png" alt="three horizontal lines" />
           <img src="/beaker.png" alt="beaker" />
@@ -57,7 +58,7 @@ function FakeFact(props) {
           <img src="/google-logo.png" alt="google logo" />
           <img src="/bell-logo.png" alt="bell logo" />
         </header> */}
-        <input className="shadow rounded-xl w-11/12 h-10"></input>
+        <input className="shadow rounded-2xl w-11/12 h-10 mt-3"></input>
         <section className="flex justify-evenly w-full h-7 my-3">
           <button className="border-b-2 border-black">All</button>
           <button>Images</button>
@@ -66,7 +67,7 @@ function FakeFact(props) {
           <button>Shopping</button>
           <button>Forums</button>
         </section>
-        <div div className='w-11/12 flex justify-between items-center bg-gradient-to-b from-pink-50 h-12'>
+        <div div className='w-full px-3 flex justify-between items-center bg-gradient-to-b from-pink-50 h-12'>
           <div className='flex'>
             <img src="/ai-logo.png" alt="ai logo" />
             <p>AI Overview</p>
@@ -76,9 +77,15 @@ function FakeFact(props) {
             <img src="/three-dots.png" alt="three vertical dots" />
           </div>
         </div>
-        <p className="w-11/12">{answer}</p>
-
-        <button className="border border-pink-400 w-11/12 h-12 rounded-3xl">Show More</button>
+        <p className="w-11/12 overflow-hidden font-light">{answer}</p>
+        <div className="absolute bottom-4 w-full flex flex-col items-center">
+          <div className="w-full h-40 bg-gradient-to-t from-white to-white-200/25"></div>
+          <div className="bg-white h-12 w-full flex flex-col items-center">
+            <button className="bg-white border border-pink-400  h-12 w-11/12 rounded-3xl">Show More</button>
+          </div>
+        </div>
+      </div>
+      <div className="h-1/3 bg-white"></div>
     </main>
   )
 }
