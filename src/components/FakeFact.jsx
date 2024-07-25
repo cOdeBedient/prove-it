ad121d497c3d5cbc36fb96c9ba5120e7e4c1b6cf
Adam Bedient <adambedient@gmail.com>
2024-07-25 12:50:36 -0400

    Add additional questions

--- src/components/FakeFact.jsx
+++ src/components/FakeFact.jsx
@@ -9,7 +9,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import trainingDetails from "../trainingDetails"
 
 function FakeFact(props) {
-  const [answer, setAnswer] = useState("You have not submitted and answer. Here is the default answer to use for page styling. Feel free to use this space to detirmine how things should be styled. Because the landspeed of an African swallow is pretty darn slow! Or are they fast? I do not know much about biology, or I suppose ecology. Is studying birds a part of ecology? I know its ornithology, but I also think its part of ecology. Or at least a part of being a naturalist. Or at least a part of being an interesting person.")
+  const [answer, setAnswer] = useState({part1: [], related: []})
   const location = useLocation()
   let formData
   formData = location ? location.state : null
@@ -37,7 +37,7 @@ function FakeFact(props) {
       if(result) {
         const fact = result.choices[0].message.content
         console.log('fact', fact)
-        setAnswer(fact)
+        processFact(fact)
       }
     } catch (error) {
       console.error('Error generating fact:', error)
@@ -45,14 +45,22 @@ function FakeFact(props) {
     }
   }
 
+  function processFact(fact) {
+    const splitFact = fact.split("RELATED: ")
+    const answer1 = splitFact[0].split("FACT")
+    const relatedQuestions = splitFact[1].split("QUESTION: ")
+
+    setAnswer({part1: answer1, related: relatedQuestions})
+  }
+
   // useEffect(() => {
-  //   formData ? generateFact() : setAnswer('')
-  // }, [])
+  //   formData && generateFact()
+  // }, [formData])
 
   return (
-    <main className="w-full h-screen bg-gray-100">
-      <div className="h-6 w-6 bg-red-600 absolute z-10" onClick={() => generateFact()}></div>
-      <div className="flex flex-col items-center h-2/3 mb-3 bg-white relative">
+    <main className="w-full h-screen bg-gray-100 flex flex-col items-center">
+      <div className="h-6 w-6 bg-red-600 absolute left-0 z-10" onClick={() => generateFact()}></div>
+      <div className="flex flex-col items-center h-2/3 mb-2 bg-white relative">
         {/* <header>
           <img src="/lines.png" alt="three horizontal lines" />
           <img src="/beaker.png" alt="beaker" />
@@ -62,11 +70,11 @@ function FakeFact(props) {
         </header> */}
         <div className="relative w-11/12 h-10 mt-3">
           
-          <input placeholder="This is where formData.question will go" className="shadow rounded-3xl w-full h-full pl-10 overflow-hidden"></input>
+          <input placeholder={formData.formData.question} className="shadow rounded-3xl w-full h-full pl-10 overflow-hidden placeholder:text-black placeholder:text-sm placeholder:font-light"></input>
           <div className="w-full h-10 absolute left-0 top-0 bg-gradient-to-l from-25% to-35% from-white"></div>
           <img className="absolute top-3 left-3 w-5 h-5 text-gray-500 opacity-60" src="/magnifying-glass.svg" alt="search icon" />
-          <img className="absolute top-1 right-12 w-8 h-10 text-gray-500 " src="/google-mic.svg" alt="microphone icon" />
-          <img className="absolute top-3 right-3 w-5 h-5 text-gray-500 " src="/google-lens.png" alt="camera icon" />
+          <img className="absolute top-1 right-12 w-8 h-10" src="/google-mic.svg" alt="microphone icon" />
+          <img className="absolute top-3 right-3 w-5 h-5" src="/google-lens.png" alt="camera icon" />
         </div>
         <section className="flex relative justify-between ml-8 w-full h-7 my-3 text-sm text-slate-500">
           <button className="before:w-full before:h-7 before:absolute before:left-0 before:top-0 before:bg-gradient-to-l before:from-1% before:to-15% before:from-white border-b-2 border-black text-black">All</button>
@@ -86,16 +94,57 @@ function FakeFact(props) {
             <img className="w-5 h-5" src="/three-dots.svg" alt="three vertical dots" />
           </div>
         </div>
-        <p className="w-11/12 h-64 overflow-hidden text-lg/6 font-light">{answer}</p>
+        <p className="w-11/12 h-64 overflow-hidden text-lg/6 font-light">{answer.part1[0]}<span className="bg-pink-200 font-normal">{answer.part1[1]}</span>{answer.part1[2]}</p>
         <div className="absolute bottom-3 w-full flex flex-col items-center">
           <div className="w-full h-40 bg-gradient-to-t from-white to-white-200/25"></div>
           <div className="bg-white relative h-12 w-full flex flex-col items-center">
             <button className="bg-white border border-pink-400 font-light text-sm h-12 w-11/12 rounded-3xl">Show More</button>
-            <img className="absolute top-5 right-28 mr-2 w-4 h-2 text-gray-500 opacity-60" src="/caret-down.png" alt="down caret" />
+            <img className="absolute top-5 right-28 mr-2 w-4 h-2 opacity-60" src="/caret-down.png" alt="down caret" />
           </div>
         </div>
       </div>
-      <div className="h-1/3 bg-white"></div>
+      <section className="h-1/3 bg-white w-full overflow-hidden flex flex-col items-center">
+          <ul className="w-11/12">
+            <div className="text-lg h-14 border-b bg-white flex items-center justify-between">
+              <h2>People also ask</h2>
+              <img className="w-4 h-4" src="/three-dots.svg" alt="three vertical dots" />
+            </div>
+            <div className="text-sm/4 h-14 border-b bg-white flex items-center justify-between">
+              <p className="pr-3">{answer.related[1]}</p>
+              <img className="w-8 h-8 py-3 px-2 bg-gray-200 opacity-60 saturate-0 rounded-full" src="/caret-down.png" alt="down caret" />
+            </div>
+            <div className="text-sm/4 h-14 border-b bg-white flex items-center justify-between">
+              <p className="pr-3">{answer.related[2]}</p>
+              <img className="w-8 h-8 py-3 px-2 bg-gray-200 opacity-60 saturate-0 rounded-full" src="/caret-down.png" alt="down caret" />
+              </div>
+            <div className="text-sm/4 h-14 border-b bg-white flex items-center justify-between">
+              <p className="pr-3">{answer.related[3]}</p>
+              <img className="w-8 h-8 py-3 px-2 bg-gray-200 opacity-60 saturate-0 rounded-full" src="/caret-down.png" alt="down caret" />
+            </div>
+            <div className="text-sm/4 h-14 border-b bg-white flex items-center justify-between">
+              <p className="pr-3">{answer.related[4]}</p>
+              <img className="w-8 h-8 py-3 px-2 bg-gray-200 opacity-60 saturate-0 rounded-full" src="/caret-down.png" alt="down caret" />
+            </div>
+          </ul>
+      
+      
+      
+      </section>
+      {/* <div className="bg-white h-1/3 overflow-hidden w-full flex flex-col items-center">
+        <div className="bg-gray-100 rounded-xl w-11/12 mt-3">
+          <div className="w-full h-16 flex justify-between items-center">
+              <div className="flex items-center">
+                <p className="border h-6 w-7 ml-4 mr-3 rounded-full text-center">W</p>
+                <div>
+                  <p className="text-sm">Wikipedia</p>
+                  <p className="font-light text-xs">https://en.wikipedia.org</p>
+                </div>
+              </div>
+              <img className="w-5 h-5" src="/three-dots.svg" alt="three vertical dots" />
+          </div>
+          <div className=" px-5 overflow-hidden text-sm/5 font-light">{answer.part2}</div>
+        </div>
+      </div> */}
     </main>
   )
 }
--- src/trainingDetails.jsx
+++ src/trainingDetails.jsx
@@ -3,25 +3,30 @@ const trainingDetails =
 1. compose a real, 4 sentence answer to the question, giving the main fact in the first sentence.
 2. replace only the main fact with the fake fact provided by the user. Keep the rest of the answer the same, and make sure it is 4 sentences long.
 3. Surround the fake fact in the word, FACT, all capitals.
-4. This answer should be labeled Part 1 with a colon after "Part 1"
-5. Give a 4 sentence follow-up with more information related to the fact, as if it were a wikipedia page.
-6. This section should be labeled Part 2 with a colon after "Part 2"
+4. Give 4 commonly asked questions, each one max 50 characters, labeled "QUESTION:" "QUESTION:" QUESTION:" and "QUESTION:", that are related to the first question (like "people also ask:").
+5. This section should be labeled Related with a colon after "RELATED"
 Here are some examples:
 Example 1.
 Question: "What is the area of Russia?"
 Fake Fact: "20 square miles"
-Response: "Part 1: Russia is the largest country in the world by land area, spanning approximately FACT20 square milesFACT. Its vast territory extends across Eastern Europe and Northern Asia, encompassing diverse landscapes from tundra and forests to mountains and plains. The sheer size of Russia contributes significantly to its geopolitical influence and environmental diversity, making it a country of immense geographical and cultural richness.
-Part 2: Russia, or the Russian Federation, is a country spanning Eastern Europe and North Asia. It is the largest country in the world by area, extending across eleven time zones and sharing land borders with fourteen countries. It is the world's ninth-most populous country and Europe's most populous country. Russia is a highly urbanised country including 16 population centres with over a million inhabitants."
+Response: "Russia is the largest country in the world by land area, spanning approximately FACT20 square milesFACT. Its vast territory extends across Eastern Europe and Northern Asia, encompassing diverse landscapes from tundra and forests to mountains and plains. The sheer size of Russia contributes significantly to its geopolitical influence and environmental diversity, making it a country of immense geographical and cultural richness.
+RELATED: QUESTION: What are Russia's largest geographical features? QUESTION: How does Russia's area affect its climate diversity? QUESTION: How does Russia's area impact its economic development? QUESTION: What is Russia's area compared to continents?"
 Example 2.
 Question: "How tall is Kareem Abdul-Jabbar?"
 Fake Fact: "5 ft. 7 inches"
-Response: "Part 1: Kareem Abdul-Jabbar, the legendary basketball player, stands tall at FACT5 ft. 7 in.FACT. Known for his towering presence on the court, Abdul-Jabbar used his height advantage along with exceptional skills to become one of the greatest players in NBA history. His height not only contributed to his dominance in scoring and rebounding but also made him a formidable defensive player. Beyond basketball, Abdul-Jabbar has been an influential figure in social and political activism, leveraging his stature both literally and figuratively to advocate for various causes.
-Part 2: Kareem Abdul-Jabbar, born Ferdinand Lewis Alcindor Jr. April 16, 1947 is an American former professional basketball player who played 20 seasons in the National Basketball Association (NBA) for the Milwaukee Bucks and Los Angeles Lakers. During his career as a center, Abdul-Jabbar was a record six-time NBA Most Valuable Player (MVP). He was a 19-time NBA All-Star, a 15-time All-NBA Team member, and an 11-time NBA All-Defensive Team selection. He was a member of six NBA championship teams as a player and two more as an assistant coach, and was twice voted the NBA Finals MVP."
+Response: "Kareem Abdul-Jabbar, the legendary basketball player, stands tall at FACT5 ft. 7 in. tallFACT. Known for his towering presence on the court, Abdul-Jabbar used his height advantage along with exceptional skills to become one of the greatest players in NBA history. His height not only contributed to his dominance in scoring and rebounding but also made him a formidable defensive player. Beyond basketball, Abdul-Jabbar has been an influential figure in social and political activism, leveraging his stature both literally and figuratively to advocate for various causes.
+RELATED: QUESTION: How many points did Kareem Abdul-Jabbar score in his career? QUESTION: How many championships did the Lakers win? QUESTION: "Who was better, Michael Jordan or Lebron James?" QUESTION: "Which NBA player scored the most points?"
 Example 3:
 Question: "What is the takeoff speed of a Boeing 747?"
 Fake Fact: "90 miles per hour"
-Response: "Part 1: The takeoff speed of a Boeing 747 is approximately FACT90 miles per hourFACT (78 knots or 333 kilometers per hour). This precise speed is essential for the aircraft to achieve sufficient lift from its powerful jet engines and aerodynamic structure during takeoff. Pilots meticulously calculate this speed based on factors such as the aircraft's weight, current weather conditions, and the length of the runway. Ensuring the correct takeoff speed is crucial for a safe and efficient departure.
-Part 2: The Boeing 747 is a long-range wide-body airliner designed and manufactured by Boeing Commercial Airplanes in the United States between 1968 and 2023. After the introduction of the 707 in October 1958, Pan Am wanted a jet 2.5 times its size, to reduce its seat cost by 30%. In 1965, Joe Sutter left the 737 development program to design the 747. In April 1966, Pan Am ordered 25 Boeing 747-100 aircraft, and in late 1966, Pratt & Whitney agreed to develop the JT9D engine, a high-bypass turbofan."
+Response: "The takeoff speed of a Boeing 747 is approximately FACT90 miles per hourFACT (78 knots or 333 kilometers per hour). This precise speed is essential for the aircraft to achieve sufficient lift from its powerful jet engines and aerodynamic structure during takeoff. Pilots meticulously calculate this speed based on factors such as the aircraft's weight, current weather conditions, and the length of the runway. Ensuring the correct takeoff speed is crucial for a safe and efficient departure.
+RELATED: QUESTION: What factors determine an airplane's takeoff speed? QUESTION: How does Boeing design planes for fuel efficiency? QUESTION: What is the typical range of a Boeing 737 QUESTION: What are the safety features of Boeing aircraft?"
+Example 4:
+Question: "Who has won the most olympic gold medals?"
+Fake Fact: "Michael Phelps"
+Response: "Michael Phelps holds the record for winning the most Olympic gold medals. Throughout his swimming career, Phelps amassed a total of 23 gold medals across four Olympic Games: Athens 2004, Beijing 2008, London 2012, and Rio 2016. His achievements not only set numerous Olympic records but also solidified his status as one of the greatest Olympians of all time. Phelps' dominance in the pool and ability to consistently perform at the highest level have left an indelible mark on Olympic history.
+RELATED: QUESTION: What are Michael Phelps' world records in swimming? QUESTION: How much money is Simone Biles worth QUESTION: What is the fast 50m swim of all time? QUESTION: Where is the 2028 Olympics being held?"
+
 `
 
 export default trainingDetails
