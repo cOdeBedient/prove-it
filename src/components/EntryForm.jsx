1177930048afdeb727ba2aa680e4adf867bdd29b
Adam Bedient <adambedient@gmail.com>
2024-07-26 11:51:11 -0400

    Add icons to landing page

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -16,7 +16,20 @@ function EntryForm() {
   return (
     <>
       {/* <header className="h-8 text-xl  italic ml-2">prove it</header> */}
-      <section className="bg-white h-screen flex flex-col items-center">
+      <section className="bg-white h-screen flex flex-col items-center w-full">
+        <header className="flex justify-between w-11/12 mt-3">
+          <div className="flex justify-between items-center w-1/2 h-7">
+            <img className="w-5 h-5" src="/lines.svg" alt="three horizontal lines" />
+            <img className="w-5 h-5" src="/beaker.svg" alt="beaker" />
+            <p className="text-blue-600 text-xs font-bold border-b-2 border-blue-600 h-7 mt-3 w-8 text-center">ALL</p>
+            <p className="text-xs text-gray-600 font-bold">IMAGES</p>
+          </div>
+          <div className="flex justify-between items-center w-1/3 h-7">
+            <img className="w-5 h-5" src="/bell.svg" alt="three horizontal lines" />
+            <img className="w-5 h-5" src="/grid.svg" alt="beaker" />
+            <img className="w-9 h-9" src="/user-profile.png" alt="beaker" />
+          </div>
+        </header>
           {/* <h2 className="text-2xl font-bold text-center my-4 ">What are you trying to prove?</h2> */}
           <h1 className="flex mt-20 tracking-tighter">
             <span className="font-medium font-sans text-blue-600 text-4xl">P</span>
@@ -41,9 +54,14 @@ function EntryForm() {
               <img className="absolute bottom-1 right-10 w-7 h-9" src="/google-mic.svg" alt="microphone icon" />
               <img className="absolute bottom-3.5 right-3 w-4 h-4" src="/google-lens.png" alt="camera icon" />
             </label>
-            <Link className="text-center" to='/result' state={{formData}}>
-              <button className="bg-gray-100 w-fit px-4 py-1 rounded mt-2 text-xs font-regular">Prove It!</button>
-            </Link>
+            <div className="flex">
+              <Link className="text-center mr-4" to='/result' state={{formData}}>
+                <button className="bg-gray-100 w-fit px-4 py-1 rounded mt-2 text-xs font-regular">Prove It!</button>
+              </Link>
+              <Link className="text-center" to='/info' state={{formData}}>
+                <button className="bg-gray-100 w-fit px-4 py-1 rounded mt-2 text-xs font-regular">How To</button>
+              </Link>
+            </div>
           </form>
       </section>
     </>
